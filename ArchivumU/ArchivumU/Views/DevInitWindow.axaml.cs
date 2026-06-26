f32c4f73dc48eb6aad7927449c41fcbce178be2e
Serinder <serinder@163.com>
2026-06-26 18:41:23 +0800

    devices init window created

---
+++ ArchivumU/ArchivumU/Views/DevInitWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ArchivumU.Views;
+
+public partial class DevInitWindow : Window
+{
+    public DevInitWindow()
+    {
+        InitializeComponent();
+    }
+}
